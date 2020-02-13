dfb9feb380cbc2ebdfd780c7b6c0655c9875b133
kumarvaibhav45 <wave.vaibhav@gmail.com>
2020-02-13 21:33:24 +0530

    Updates

---
+++ precache-manifest.8ffb98be681fb0880baaf2c56aa3781a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b3888078318435767502f5ccd95bdf",
+    "url": "/border-radius-previewer/index.html"
+  },
+  {
+    "revision": "236805c05ac4ee42e3b6",
+    "url": "/border-radius-previewer/static/css/main.cd5be5f3.chunk.css"
+  },
+  {
+    "revision": "04a909ee7d208e1b5d6b",
+    "url": "/border-radius-previewer/static/js/2.e52ae0f9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/border-radius-previewer/static/js/2.e52ae0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236805c05ac4ee42e3b6",
+    "url": "/border-radius-previewer/static/js/main.e23ea489.chunk.js"
+  },
+  {
+    "revision": "ab0bea932b458f82dba0",
+    "url": "/border-radius-previewer/static/js/runtime-main.103214e3.js"
+  }
+]);
